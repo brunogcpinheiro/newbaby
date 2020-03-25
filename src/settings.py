ad8d971930407af3cfb28c5a77ba47d4fc711aee
Bruno Pinheiro <brunogcpinheiro@gmail.com>
2020-03-25 19:52:09 -0300

    fix: remove www from src settings installed apps

--- src/settings.py
+++ src/settings.py
@@ -36,8 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'www.apps.WwwConfig'
+    'django.contrib.staticfiles'
 ]
 
 MIDDLEWARE = [
