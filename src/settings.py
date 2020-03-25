1b41a8d5ad17dab226c686e19213e0a1a3ce7749
Bruno Pinheiro <brunogcpinheiro@gmail.com>
2020-03-25 19:50:24 -0300

    fix: remove www app and create views on src app

--- src/settings.py
+++ src/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'www'
+    'www.apps.WwwConfig'
 ]
 
 MIDDLEWARE = [
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'src.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'newbaby',
+        'USER': 'docker',
+        'PASSWORD': 'docker',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
