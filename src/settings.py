64ed9d5e7e0d5006a5739c1830e9d8306bfd6348
Bruno Pinheiro <brunogcpinheiro@hotmail.com>
2020-04-07 20:19:00 +0000

    feat: image and profile about page

--- src/settings.py
+++ src/settings.py
@@ -75,12 +75,8 @@ WSGI_APPLICATION = 'src.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'newbaby',
-        'USER': 'docker',
-        'PASSWORD': 'docker',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
